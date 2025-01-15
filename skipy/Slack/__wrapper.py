95bdff42b1292ff204f48b02740a7d358f5a244d
syamaguc <programmer2844@gmail.com>
2025-01-15 22:40:34 +0900

    chore

--- skipy/Slack/__wrapper.py
+++ skipy/Slack/__wrapper.py
@@ -1,6 +1,43 @@
 import json
 
 import requests
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+
+class SlackWebClient:
+    def __init__(self, token):
+        self.client = WebClient(token=token)
+
+    def upload_file(self, channel_id, file_path, title=None, initial_comment=None):
+        try:
+            response = self.client.files_upload_v2(
+                channel=channel_id,
+                file=file_path,
+                title=title,
+                initial_comment=initial_comment,
+            )
+            print(f"File uploaded successfully. File ID: {response['file']['id']}")
+        except SlackApiError as e:
+            print(f"Error uploading file: {e.response['error']}")
+
+    def post_snippet(
+        self,
+        channel_id,
+        text_snippet="This is test snippet",
+        title=None,
+        initial_comment=None,
+    ):
+        try:
+            response = self.client.files_upload_v2(
+                channel=channel_id,
+                content=text_snippet,
+                title=title,
+                initial_comment=initial_comment,
+            )
+            print(f"File uploaded successfully. File ID: {response['file']['id']}")
+        except SlackApiError as e:
+            print(f"Error uploading file: {e.response['error']}")
 
 
 class Slack:
